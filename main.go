0f20d468dc04b6a889ff5818094360e87333ff04
Gabriel <52558343+gabrielleeyj@users.noreply.github.com>
2020-11-29 16:24:02 +0800

    Update main.go

--- main.go
+++ main.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 // Add Function - takes in 2 variables of value int. returns an int.
 func Add(a, b int) int {
 	return a + b
@@ -8,4 +10,9 @@ func Add(a, b int) int {
 // Subtract Function - takes in 2 variables of value int. returns an int.
 func Sub(a, b int) int {
 	return a - b
+}
+
+func main() {
+	fmt.Println(Add(3,4))
+	fmt.Println(Sub(4,3))
 }
