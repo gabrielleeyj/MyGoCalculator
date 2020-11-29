fc94476250363d6f2ddd611f14d9e3ed11b369fa
Gabriel <52558343+gabrielleeyj@users.noreply.github.com>
2020-11-29 16:25:27 +0800

    Update main_test.go

--- main_test.go
+++ main_test.go
@@ -11,8 +11,8 @@ func TestAddition(t *testing.T) {
 	if result < 0 {
 		t.Error("Value is negative")
 	}
-	if result != 3 {
-		t.Errorf("%d + %d = %d, Value is not 3", a, b, result)
+	if result != 4 {
+		t.Errorf("%d + %d = %d, Value is not 4", a, b, result)
 	} else {
 		fmt.Printf("Result : %d is correct!", result)
 	}
